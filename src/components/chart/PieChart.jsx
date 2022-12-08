6ead6100f884cb27bbe8593fb9bda1001feed384
SoleSM <miliserrrano@gmail.com>
2022-12-08 10:43:41 -0300

    reportes graficos

---
+++ src/components/chart/PieChart.jsx
@@ -0,0 +1,74 @@
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, Title, BarElement } from "chart.js";
+import { Pie, Bar } from "react-chartjs-2";
+
+ChartJS.register(
+    ArcElement,
+    Tooltip,
+    Legend,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title
+);
+
+const labels = ["Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+export const PieChartReporte = ({ dataTotal }) => {
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                data: dataTotal,
+                backgroundColor: ["aqua", "orange", "purple", "yellow", "blue", "green", "pink"]
+            }
+        ]
+    };
+
+    const options = {
+        responsive: true
+    }
+    return (
+
+        <Pie width={600} height={600} data={data} options={options} />
+
+    )
+};
+
+export const BarChartReporte = ({ dataTotal, cantVentas }) => {
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Gráfico total de ventas y montos por mes',
+            },
+        },
+    };
+
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'Cantidad de Ordenes',
+                data: cantVentas,
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+            {
+                label: 'Total $',
+                data: dataTotal,
+                backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            },
+        ],
+    };
+
+    return (
+        <Bar data={data} options={options} />
+    )
+
+}
--- src/views/dashboard/reportes/reportes.jsx
+++ src/views/dashboard/reportes/reportes.jsx
@@ -3,33 +3,74 @@ import "./reportes.scss"
 import { TablaReporte } from "../../../components/reporte/tablaReporte";
 import Pdf from "react-to-pdf";
 import { createRef } from "react";
+import { BarChartReporte, PieChartReporte } from "../../../components/chart/PieChart";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useEffect } from "react";
 
 
 const ref = createRef();
 const options = {
     orientation: 'landscape',
-    
 };
 
 export const ReportesView = () => {
 
+    const { comparacionSeisUltimosMeses } = useSelector(state => state.statsReducer);
+    const [dataIngresos, setDataIngresos] = useState([]);
+
+    useEffect(() => {
+        setDataIngresos(comparacionSeisUltimosMeses);
+    })
+
+    let arrayValoresTotales = [];
+    let arrayValoresVentas = [];
+    for (let key in dataIngresos) {
+        arrayValoresTotales.push(dataIngresos[key].total);
+        arrayValoresVentas.push(dataIngresos[key].cantVentas);
+    }
+    // console.log("array", arrayValoresTotales);
+    // console.log("arrayCantVentas", arrayValoresVentas)
+
     return (
         <>
-    
+
             <div className="container-table-report">
-                <div className="datatableTitle">
-                    Reportes
-                </div>
-                <div ref={ref}>
-                    <div className="titleDataReporte">
-                        Ventas en los últimos seis meses
+                <div className="titulo-y-boton">
+                    <div className="datatableTitle">
+                        Reportes Ventas
                     </div>
-                    <TablaReporte />
+                    <Pdf targetRef={ref} filename="documentReporte.pdf" options={options} x={10} y={10} scale={0.8}>
+                        {({ toPdf }) => <button className="button-reporte" onClick={toPdf}>Descargar PDF</button>}
+                    </Pdf>
                 </div>
 
-                <Pdf targetRef={ref} filename="document.pdf" options={options} x={10} y={10} scale={0.8}>
-                    {({ toPdf }) => <button className="button-reporte" onClick={toPdf}>Descargar PDF</button>}
-                </Pdf>
+                <div ref={ref}>
+                    <div className="titleDataReporte">
+                        En los últimos seis meses
+                    </div>
+
+
+                    <TablaReporte />
+
+                    <div className="containerCharts">
+                        <div style={{ "width": "40%" }}>
+                            <PieChartReporte dataTotal={arrayValoresTotales} />
+                        </div>
+                        <div style={{ "width": "55%", "margin": "auto" }}>
+                            <BarChartReporte
+                                cantVentas={arrayValoresVentas}
+                                dataTotal={arrayValoresTotales}
+                            />
+                        </div>
+                    </div>
+
+
+                </div>
+
+
+
+
 
             </div>
 
